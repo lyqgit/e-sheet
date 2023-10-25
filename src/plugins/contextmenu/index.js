68e7af4dae4c66d3c5451b8095f3d975c8f5052a
lyq <upt3195@dingtalk.com>
2023-10-25 09:45:38 +0800

    feat：拆分单元格功能

--- src/plugins/contextmenu/index.js
+++ src/plugins/contextmenu/index.js
@@ -20,6 +20,72 @@ export default class ContextmenuPlugin{
         this.containerDom.style.display = 'none'
     }
 
+    splitCell=()=>{
+        const { clickCell } = this.contentComponent
+        if(!clickCell.isMerge){
+            return
+        }
+
+        clickCell.mergeEndLabel = ''
+        clickCell.mergeStartLabel = ''
+        clickCell.isMerge = false
+
+        for(let i=clickCell.row,ni=clickCell.row+clickCell.mergeRow;i<ni;i++){
+            for(let j=clickCell.col,nj=clickCell.col+clickCell.mergeCol;j<nj;j++){
+                // console.log('clickCell.col',i,j)
+                if(i===clickCell.row && j===clickCell.col){
+
+                }else{
+                    const tempRect = this.contentComponent.searchRectByColAndRow(j,i)
+                    tempRect.mergeEndLabel = ''
+                    tempRect.mergeStartLabel = ''
+                    tempRect.isMerge = false
+                }
+            }
+        }
+
+        clickCell.mergeRow = 1
+        clickCell.mergeCol = 1
+        this.contentComponent.showClickRect(clickCell)
+        this.core.fresh()
+        this.hideContextMenu()
+    }
+
+    mergeCell=()=>{
+        const { clickCell,mergeSelectedCell } = this.contentComponent
+        if(mergeSelectedCell.some(item=>item.isMerge)){
+            return
+        }
+        let mergeWidth = clickCell.width
+        let mergeHeight = clickCell.height
+        // clickCell.mergeLabelGroup = mergeSelectedCell
+        clickCell.mergeEndLabel = mergeSelectedCell[mergeSelectedCell.length - 1].label
+        clickCell.mergeStartLabel = clickCell.label
+        clickCell.isMerge = true
+        clickCell.mergeRow = 1
+        clickCell.mergeCol = 1
+        mergeSelectedCell.forEach(item=> {
+            item.isMerge = true
+            item.mergeStartLabel = clickCell.label
+            item.mergeEndLabel = mergeSelectedCell[mergeSelectedCell.length - 1].label
+            if(clickCell.row === item.row && clickCell.label !== item.label){
+                mergeWidth+=item.width
+                clickCell.mergeCol += 1
+            }
+            if(clickCell.col === item.col && clickCell.label !== item.label){
+                mergeHeight+=item.height
+                clickCell.mergeRow += 1
+            }
+
+        })
+        clickCell.mergeWidth = mergeWidth
+        clickCell.mergeHeight = mergeHeight
+        // console.log('合并完成',clickCell)
+        this.contentComponent.setSecondClickCell(null)
+        this.core.freshContent()
+        this.hideContextMenu()
+    }
+
     registryContextMenu(){
         const containerDom = this.core.h('div',{
             style:{
@@ -27,6 +93,24 @@ export default class ContextmenuPlugin{
             },
             attr:{
                 className:'contextmenu-layout'
+            }
+        })
+
+        const mergeBtn = this.core.h('div',{
+            style:{
+                cursor:'pointer'
+            },
+            attr:{
+                innerText:'合并单元格'
+            }
+        })
+
+        const splitBtn = this.core.h('div',{
+            style:{
+                cursor:'pointer'
+            },
+            attr:{
+                innerText:'拆分单元格'
             }
         })
 
@@ -43,54 +127,29 @@ export default class ContextmenuPlugin{
                 containerDom.style.display = 'flex'
                 containerDom.style.left = clickCell.ltX-offsetX+cellHeight+'px'
                 containerDom.style.top = clickCell.ltY-offsetY+'px'
+                if(clickCell.isMerge){
+                    mergeBtn.style.display='none'
+                    splitBtn.style.display='block'
+                }else{
+                    splitBtn.style.display='none'
+                    mergeBtn.style.display='block'
+
+                }
             }
         })
 
-        const mergeBtn = this.core.h('div',{
-            style:{
-                cursor:'pointer'
-            },
-            attr:{
-                innerText:'合并单元格'
-            }
-        })
+
 
         mergeBtn.onclick = _=>{
-            const { clickCell,mergeSelectedCell } = this.contentComponent
-            if(mergeSelectedCell.some(item=>item.isMerge)){
-                return
-            }
-            let mergeWidth = clickCell.width
-            let mergeHeight = clickCell.height
-            // clickCell.mergeLabelGroup = mergeSelectedCell
-            clickCell.mergeEndLabel = mergeSelectedCell[mergeSelectedCell.length - 1].label
-            clickCell.mergeStartLabel = clickCell.label
-            clickCell.isMerge = true
-            clickCell.mergeRow = 1
-            clickCell.mergeCol = 1
-            mergeSelectedCell.forEach(item=> {
-                item.isMerge = true
-                item.mergeStartLabel = clickCell.label
-                item.mergeEndLabel = mergeSelectedCell[mergeSelectedCell.length - 1].label
-                if(clickCell.row === item.row && clickCell.label !== item.label){
-                    mergeWidth+=item.width
-                    clickCell.mergeCol += 1
-                }
-                if(clickCell.col === item.col && clickCell.label !== item.label){
-                    mergeHeight+=item.height
-                    clickCell.mergeRow += 1
-                }
+            this.mergeCell()
+        }
 
-            })
-            clickCell.mergeWidth = mergeWidth
-            clickCell.mergeHeight = mergeHeight
-            // console.log('合并完成',clickCell)
-            this.contentComponent.setSecondClickCell(null)
-            this.core.freshContent()
-            this.hideContextMenu()
+        splitBtn.onclick = _=>{
+            this.splitCell()
         }
 
         containerDom.appendChild(mergeBtn)
+        containerDom.appendChild(splitBtn)
         this.selectorDom.appendChild(containerDom)
     }
 
