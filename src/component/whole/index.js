ec45e8ad865124ccfe72992a7b5e538b93138179
lyq <upt3195@dingtalk.com>
2023-10-27 16:10:46 +0800

    fix：补充左上角边框线

--- src/component/whole/index.js
+++ src/component/whole/index.js
@@ -7,23 +7,28 @@ export default class WholeComponent{
      * @type {Canvas}
      */
     layer = null
+    core = null
 
-    constructor(layer,options={}) {
+    constructor(layer,options={},core) {
         this.layer = layer
         this.options = options;
+        this.core = core;
         this.draw();
         // this.addEvent();
     }
 
     draw(){
 
+        const { borderCellBgColor,borderColor } = this.core
         const { cellHeight } = this.options
 
         this.layer.clearRect(0,0,cellHeight,cellHeight)
         this.layer.drawTriangleRect(
             {x:cellHeight-6,y:6},{x:cellHeight-6,y:cellHeight-6},{x:6,y:cellHeight-6}
             ,'#DCDCDC')
-        this.layer.drawFillRect(0,0,cellHeight,cellHeight,'#F9FBFD','destination-over')
+        this.layer.drawLine([0,0,cellHeight,0],null,borderColor)
+        this.layer.drawLine([0,0,0,cellHeight],null,borderColor)
+        this.layer.drawFillRect(0,0,cellHeight,cellHeight,borderCellBgColor,'destination-over')
     }
 
     addEvent(){
